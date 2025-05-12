b211dff68fbb5ed46844b1a804a4a6cdda87e6b6
Vishnuteja229 <vishnutejakotagiri22@gmail.com>
2025-05-12 20:08:21 +0530

    Data validation

---
+++ src/pipline/prediction_pipeline.py
@@ -0,0 +1,6 @@
+import sys
+from src.entity.config_entity import VehiclePredictorConfig
+from src.entity.s3_estimator import Proj1Estimator
+from src.exception import MyException
+from src.logger import logging
+from pandas import DataFrame
